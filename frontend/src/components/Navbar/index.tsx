4dcc3e8ae036430dc5aa58f6376495135c13ccf4
Slip <zuraffa2015@gmail.com>
2022-05-10 01:04:44 -0300

    dia fechdo - componente Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+import './styles.css';
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/SlipHD">
+                        <div>
+                            <GithubIcon />
+                            <p>/SlipHD</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+
+    );
+
+}
+
+export default Navbar;
